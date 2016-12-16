787f798b95ff9cb7236736f323cc0d67b31bcec9
josh <joshua@marinacci.org>
2016-12-16 15:42:59 -0800

    update the two server components to JSv4

--- lesson1/server5.js
+++ lesson1/server5.js
@@ -4,14 +4,15 @@
 /**
  * Created by josh on 2/17/16.
  */
+/**
+ * Produce five streams of random data on the channel: random5
+ */
+var PubNub = require('pubnub');
 
-
-var pubnub = require("pubnub")({
-    ssl           : true,  // <- enable TLS Tunneling over TCP
-    publish_key   : "pub-c-5a5f3514-32a6-49a0-94d9-ea376729f959",
-    subscribe_key : "sub-c-d784e128-da7d-11e5-9511-0619f8945a4f"
+var pubnub = new PubNub({
+    publishKey   : "pub-c-5a5f3514-32a6-49a0-94d9-ea376729f959",
+    subscribeKey : "sub-c-d784e128-da7d-11e5-9511-0619f8945a4f"
 });
-
 
 var numbers = [100,100,100,100,100];
 function publish() {
@@ -24,7 +25,7 @@ function publish() {
     console.log("sending",message);
     pubnub.publish({
         channel   : 'random5',
-        message   : message,
+        message   : message
     });
 }
 setInterval(publish,3*1000); //every three seconds
